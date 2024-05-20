c2948c8a4dd8cd5f9f8f3ca2d76a993e01eec93b
ersiilva <83134364+ersiilva@users.noreply.github.com>
2024-05-20 05:01:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ document.getElementById('toggleTheme').addEventListener('click', toggleTheme);
 
 function appendNumber(number) {
     currentInput += number;
-    display.value = currentInput;
+    updateDisplay();
 }
 
 function appendOperator(operator) {
     currentInput += ` ${operator} `;
-    display.value = currentInput;
+    updateDisplay();
 }
 
 function calculate() {
@@ -20,7 +20,7 @@ function calculate() {
         let result = eval(expression);
 
         currentInput = result.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-        display.value = currentInput;
+        updateDisplay();
     } catch {
         display.value = 'Error';
     }
@@ -28,14 +28,14 @@ function calculate() {
 
 function clearDisplay() {
     currentInput = '';
-    display.value = currentInput;
+    updateDisplay();
 }
 
 function printResult() {
     const result = display.value;
     const description = document.getElementById('description').value;
     if (result) {
-        const printContent = `\n------PAUD'ARCO------\n\n&nbsp;&nbsp;&nbsp;&nbsp;${description}\n\n\n\n&nbsp;&nbsp;TOTAL: R$ ${result}\n\n\n&nbsp;${new Date().toLocaleString('pt-BR')}\n----------------------`;
+        const printContent = `\n------PAUD'ARCO------\n\n\n&nbsp;&nbsp;&nbsp;&nbsp;${description}\n\n\n&nbsp;&nbsp;TOTAL: R$${result}\n\n\n${new Date().toLocaleString('pt-BR')}\n----------------------`;
         
         if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
             const printWindow = window.open('', '_blank', 'width=300,height=200');
@@ -62,13 +62,18 @@ function handleKeyboardInput(event) {
         calculate();
     } else if (key === 'Backspace') {
         currentInput = currentInput.slice(0, -1);
-        display.value = currentInput;
+        updateDisplay();
     } else if (key === 'Escape') {
         clearDisplay();
     } else if (event.ctrlKey && (key === 'p' || key === 'P')) {
         printResult();
-        event.preventDefault(); // Evita o comportamento padrão de abrir a janela de impressão do navegador
+        event.preventDefault();
     }
+}
+
+function updateDisplay() {
+    display.value = currentInput;
+    display.scrollLeft = display.scrollWidth; // Garante que o último número digitado esteja visível
 }
 
 function toggleTheme() {
